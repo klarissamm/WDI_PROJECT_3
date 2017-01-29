153243327f473b5d7b9cbb243fa5b1ad6f72b149
savaris <hudhayfajamalkhan@gmail.com>
2017-01-29 17:24:04 +0000

    Added users controllers

--- stackoverflow/src/app.js
+++
@@ -1,6 +0,0 @@
-angular
-  .module('ourApp', [
-    'ui.router',
-    'ngResource',
-    'angular-jwt'
-  ]);
---
+++ stackoverflow/src/js/constants/api.constant.js
@@ -0,0 +1,3 @@
+angular
+  .module('ourApp')
+  .constant('API', `${window.location.origin}/api`);
