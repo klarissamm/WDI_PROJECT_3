e5f18edc23ccda29521063ef9fe850fec56cddbc
jgburton <jgranthamburton@gmail.com>
2017-01-30 13:52:47 +0000

    added contollers to front end for authentication

---
+++ stackoverflow/src/js/controllers/main.controller.js
@@ -0,0 +1,11 @@
+angular
+.module('ourApp')
+.controller('MainCtrl', MainCtrl);
+
+MainCtrl.$inject = ['$rootScope', 'CurrentUserService'];
+function MainCtrl($rootScope, CurrentUserService){
+  const vm = this;
+  $rootScope.$on('loggedIn', () => {
+    vm.user = CurrentUserService.currentUser;
+  });
+}
---
+++ stackoverflow/src/js/controllers/register.contoller.js
@@ -0,0 +1,18 @@
+angular
+.module('ourApp')
+.controller('RegisterCtrl', RegisterCtrl);
+
+RegisterCtrl.$inject = ['User', 'CurrentUserService'];
+function RegisterCtrl(User, CurrentUserService){
+  const vm =this;
+
+  vm.register = () => {
+    User.register(vm.user)
+    .$promise
+    .then(() => {
+      CurrentUserService.getUser();
+    }, err => {
+      console.log(err);
+    });
+  };
+}
